c160114c13b37fc66cad1399190f7c7af09efe1e
ishibshi.futos <ishibshi.futos@outlook.com>
2019-05-05 11:45:26 +0900

    Welcome to AtCoder

---
+++ practiceContest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace practiceContest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            string[] input = Console.ReadLine().Split(' ');
+            int b = int.Parse(input[0]);
+            int c = int.Parse(input[1]);
+            string s = Console.ReadLine();
+            Console.WriteLine((a + b + c) + " " + s);
+        }
+    }
+}
