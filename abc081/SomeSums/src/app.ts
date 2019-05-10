b8300da9659cfc898191d1794958b92d6973df71
ishibshi.futos <ishibshi.futos@outlook.com>
2019-05-10 15:11:55 +0900

    提出 #5327356, 実行時エラー

---
+++ abc081/SomeSums/src/app.ts
@@ -0,0 +1,14 @@
+import * as fs from 'fs';
+
+let Main = (i: string): number => {
+  let input = i.split(' ').map((v) => v)
+  let N = [...Array(parseInt(input[0]) + 1).keys()].filter((v) => v != 0).map((v) => v.toString())
+  let A = parseInt(input[1])
+  let B = parseInt(input[2])
+  return N.map((v) => {
+    let val = v.split('').map((v) => parseInt(v)).reduce((v, n) => v + n)
+    return A <= val && val <= B ? parseInt(v) : 0
+  }).reduce((v, x) => x += v)
+}
+
+console.log(Main(fs.readFileSync('/dev/stdin', 'utf8')))
